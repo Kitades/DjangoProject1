2a5b73407d7eb1ef8b54f36c4d55975f992e9e2d
Maksim <maks.severgin80@gmail.com>
2024-07-30 16:41:29 +0700

    firstDjangoCommit

---
+++ catalog/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        print(f'{name}, '
+              f'{phone}, '
+              f'{message} ')
+
+    return render(request, 'contacts.html')
