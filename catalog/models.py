e64c5bd86e6b68c96b755e07a7959d4c523628a5
Maksim <maks.severgin80@gmail.com>
2024-08-02 14:18:16 +0700

    makemigration2

--- catalog/models.py
+++ catalog/models.py
@@ -22,6 +22,7 @@ class Product(models.Model):
     price = models.IntegerField(verbose_name='Цена')
     created_at = models.DateTimeField(verbose_name='Дата создания')
     updated_at = models.DateTimeField(verbose_name='Дата изменения')
+    manufactured_at = models.DateTimeField(verbose_name='Дата производства продукта', null=True, blank=True)
 
     def __str__(self):
         return (f'{self.name}, {self.description}, {self.imagen}, {self.category}, {self.price}, '
